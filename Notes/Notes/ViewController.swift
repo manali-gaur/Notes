519837acc52286debd8b7da5d9cbe18afa3ea18e
Manali Gaur <managaur@in.ibm.com>
2017-03-23 16:15:47 +0530

    Initial commit

---
+++ Notes/Notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Manali Gaur on 23/03/17.
+//  Copyright © 2017 Manali Gaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
